35de2b53df4be04fefa195e1db0e6dfc3fa8ab6a
Drrek Mark <hanmark27@gmail.com>
2024-08-21 23:11:16 -0500

    iloveminros

---
+++ Ball.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java .io.*;
+public class Ball {
+    public static void main(String[] args)throws IOException{
+        Scanner sc = new Scanner(new File("lameloball.txt"));
+        long num = sc.nextLong();
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i = 0; i <num; i++)
+            arr.add((sc.nextLong()));
+        Collections.sort(arr);
+        long sum =  arr.indexOf(0) * arr.size();
+        long highest=0;
+        long index = 0;
+        for(int i=0; i<num;i++){
+           highest = arr.get(i) * (arr.size() - i);
+           if(highest> sum) {
+              index= arr.get(i);
+              sum = highest; 
+           }
+        }
+        System.out.print(sum+" "+index);
+    }
+}
